8af0c476e9a3524cc7309a5236073fcf558b0b77
lakshu <ced15i004@iiitdm.ac.in>
2022-03-02 12:58:33 +0530

    With Animations and Other

--- app/src/main/java/com/example/gradients/MainActivity.kt
+++ app/src/main/java/com/example/gradients/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gradients
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,29 +28,93 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(modifier = Modifier.fillMaxSize().wrapContentSize()) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize()
+                    ) {
 
                         // Create a linear gradient that shows red in the top left corner
                         // and blue in the bottom right corner
-                        val linear = Brush.linearGradient(listOf(Color.Red, Color.Blue))
+                        val trans1 = rememberInfiniteTransition()
+                        val trananim1 = trans1.animateFloat(
+                            initialValue = 0f,
+                            targetValue = 1000f,
+                            animationSpec = infiniteRepeatable(
+                                animation = tween(
+                                    durationMillis = 1000,
+                                    easing = FastOutSlowInEasing
+                                ),
+                                repeatMode = RepeatMode.Restart
 
-                        Box(modifier = Modifier.size(120.dp).background(linear))
+                            )
+                        )
+                        val linear = Brush.linearGradient(listOf(Color.Cyan, Color.Blue, Color.Magenta),
+                        start = Offset.Zero,
+                            end = Offset(x= trananim1.value,y= trananim1.value)
+                            )
 
-                        Spacer(modifier = Modifier.size(20.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(200.dp)
+                                .background(linear)
+                        )
+
+                        Spacer(modifier = Modifier.size(2.dp))
 
                         // Create a radial gradient centered about the drawing area that is green on
                         // the outer
                         // edge of the circle and magenta towards the center of the circle
                         val radial = Brush.radialGradient(listOf(Color.Green, Color.Magenta))
-                        Box(modifier = Modifier.size(120.dp).background(radial))
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .background(radial)
+                        )
 
-                        Spacer(modifier = Modifier.size(20.dp))
+                        Spacer(modifier = Modifier.size(2.dp))
 
                         // Create a radial gradient centered about the drawing area that is green on
                         // the outer
                         // edge of the circle and magenta towards the center of the circle
                         val sweep = Brush.sweepGradient(listOf(Color.Cyan, Color.Magenta))
-                        Box(modifier = Modifier.size(120.dp).background(sweep))
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .background(sweep)
+                        )
+                        Spacer(modifier = Modifier.size(2.dp))
+
+                        val trans = rememberInfiniteTransition()
+                        val trananim = trans.animateFloat(
+                            initialValue = 0f,
+                            targetValue = 1000f,
+                            animationSpec = infiniteRepeatable(
+                                animation = tween(
+                                    durationMillis = 1000,
+                                    easing = FastOutSlowInEasing
+                                ),
+                                repeatMode = RepeatMode.Reverse
+
+                            )
+                        )
+                        val horizontal = Brush.horizontalGradient(
+                            listOf(
+                                Color.Cyan.copy(
+                                    alpha = 0.6f
+                                ),
+                                Color.Blue.copy(
+                                    alpha = 0.9f
+                                )
+                            ),
+                            startX = Offset.Zero.x,
+                            endX = trananim.value
+                        )
+                        Box(
+                            modifier = Modifier
+                                .size(200.dp)
+                                .background(horizontal)
+                        )
                     }
                 }
             }
@@ -65,6 +131,76 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     GradientsTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+        ) {
+
+            // Create a linear gradient that shows red in the top left corner
+            // and blue in the bottom right corner
+            val linear = Brush.linearGradient(listOf(Color.Cyan, Color.Blue))
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(linear)
+            )
+
+            Spacer(modifier = Modifier.size(2.dp))
+
+            // Create a radial gradient centered about the drawing area that is green on
+            // the outer
+            // edge of the circle and magenta towards the center of the circle
+            val radial = Brush.radialGradient(listOf(Color.Green, Color.Magenta))
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(radial)
+            )
+
+            Spacer(modifier = Modifier.size(2.dp))
+
+            // Create a radial gradient centered about the drawing area that is green on
+            // the outer
+            // edge of the circle and magenta towards the center of the circle
+            val sweep = Brush.sweepGradient(listOf(Color.Cyan, Color.Magenta))
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(sweep)
+            )
+            Spacer(modifier = Modifier.size(2.dp))
+            val trans = rememberInfiniteTransition()
+            val trananim = trans.animateFloat(
+                initialValue = 0f,
+                targetValue = 1000f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = 1000,
+                        easing = FastOutSlowInEasing
+                    ),
+                    repeatMode = RepeatMode.Reverse
+
+                )
+            )
+            val horizontal = Brush.horizontalGradient(
+                listOf(
+                    Color.Cyan.copy(
+                        alpha = 0.5f
+                    ),
+                    Color.Blue.copy(
+                        alpha = 0.5f
+                    )
+                ),
+                startX = Offset.Zero.x,
+                endX = trananim.value
+            )
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .background(horizontal)
+            )
+        }
     }
 }
